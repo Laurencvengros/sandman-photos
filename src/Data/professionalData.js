d2be814c9624049f27e974a051a2293bd8216f54
Lauren <l.cvengros@icloud.com>
2023-02-10 15:36:51 -0500

    dynamically render columns for professional page

---
+++ src/Data/professionalData.js
@@ -0,0 +1,23 @@
+import React from "react";
+import GRCG from '../assets/GRCG/GRCG_header.jpg'
+import copperCoin from '../assets/copper-coin/Copper_Coin_Shop.jpg'
+
+
+ const data = [
+    {
+        id: 1,
+        name: "COPPER COIN",
+        view:"/copperCoin",
+        pic: copperCoin
+    },
+    {
+        id: 2,
+        name: "GRAND RAPIDS CITY GYM",
+        view:"/copperCoin",
+        pic: GRCG
+    },
+]
+
+export default data;
+
+
--- src/components/Professional.js
+++ src/components/Professional.js
@@ -8,8 +8,11 @@ import copperCoin from '../assets/copper-coin/Copper_Coin_Shop.jpg'
 import '../Styles/Work.css'
 import { Link } from 'react-router-dom';
 import WebFont from 'webfontloader';
-import Footer from '../components/Footer'
-import '../Styles/Home.css'
+import Footer from '../components/Footer';
+import '../Styles/Home.css';
+import data from '../Data/professionalData.js'
+
+
 
 WebFont.load({
    google: {
@@ -17,30 +20,44 @@ WebFont.load({
    }
  });
 
+// const data =[
+//     {
+//     id: 1,
+//     name: "COPPER COIN",
+//     view:'/copperCoin',
+//     pic: copperCoin,
+//     }
+// ]
+
 
 
 function Professional(){
 
 
     return(
-        <React.Fragment>
-            <Navigation/>
+    
+         <React.Fragment>
+             <Navigation/>
             <Container fluid className="containers">
-                <Row className="row">
-                    <Col className="col">
-                        <Link to="/CopperCoin">
+                
+                {data.map((data, k) => (
+                    <Row >
+                    <Col key={k} className="col">
+                        <Link to={data.view}>
                             
                             <div className="imagebox">
-                            <img className=' resize ' src={copperCoin} alt='copper coin' />
-                                <div className="caption" style={{fontFamily: 'Sofia Sans Condensed'}}>COPPER COIN</div>
-                            </div>
-                        </Link>
-                    </Col>
-                </Row>
+                             <img className=' resize ' src={data.pic} alt={data.name} />
+                                <div className="caption" style={{fontFamily: 'Sofia Sans Condensed'}}>{data.name}</div>
+                             </div>
+                         </Link>
+                     </Col>
+                     </Row>
+                ))}
+                 
                 
             </Container>
-            <Footer/>
-        </React.Fragment>
+             <Footer/>
+         </React.Fragment> 
         
     )
     
