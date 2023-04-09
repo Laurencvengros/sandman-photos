04e2b8c2054da725046994d72fddbedad75a03d5
Lauren <l.cvengros@icloud.com>
2023-04-09 16:10:18 -0400

    added norway photos

--- src/Data/norwayData.js
+++ src/Data/norwayData.js
@@ -1,11 +1,22 @@
-import norway1 from '../assets/Norway/norwayMain.jpg'
-import GRCG2 from '../assets/GRCG/GRCG_2.jpg'
-import GRCG3 from '../assets/GRCG/GRCG_3.jpg'
-import GRCG4 from '../assets/GRCG/GRCG_4.jpg'
-import GRCG5 from '../assets/GRCG/GRCG_5.jpg'
-import GRCG6 from '../assets/GRCG/GRCG_6.jpg'
-import GRCG7 from '../assets/GRCG/GRCG_7.jpg'
-import GRCG8 from '../assets/GRCG/GRCG_8.jpg'
+import norway1 from '../assets/Norway/norway-1.jpg'
+import norway2 from '../assets/Norway/norway-2.jpg'
+import norway3 from '../assets/Norway/norway-3.jpg'
+import norway4 from '../assets/Norway/norway-4.jpg'
+import norway5 from '../assets/Norway/norway-5.jpg'
+import norway6 from '../assets/Norway/norway-6.jpg'
+import norway7 from '../assets/Norway/norway-7.jpg'
+import norway8 from '../assets/Norway/norway-8.jpg'
+import norway9 from '../assets/Norway/norway-9.jpg'
+import norway10 from '../assets/Norway/norway-10.jpg'
+import norway11 from '../assets/Norway/norway-11.jpg'
+import norway12 from '../assets/Norway/norway-12.jpg'
+import norway13 from '../assets/Norway/norway-13.jpg'
+import norway14 from '../assets/Norway/norway-14.jpg'
+import norway15 from '../assets/Norway/norway-15.jpg'
+import norway16 from '../assets/Norway/norway-16.jpg'
+import norway17 from '../assets/Norway/norway-17.jpg'
+import norway18 from '../assets/Norway/norway-18.jpg'
+import norway19 from '../assets/Norway/norway-19.jpg'
 
 
 const col1 = [
@@ -16,44 +27,99 @@ const col1 = [
     },
     {
         id: 2,
-        name: "GRCG7",
-        pic: GRCG7
+        name: "norway2",
+        pic: norway2
     },
     
     {
         id: 3,
-        name: "GRCG3",
-        pic: GRCG3
+        name: "norway3",
+        pic: norway3
+    },
+    {
+        id: 4,
+        name: "norway11",
+        pic: norway11
+    },
+    {
+        id: 5,
+        name: "norway14",
+        pic: norway14
+    },
+    {
+        id: 6,
+        name: "norway17",
+        pic: norway17
     },
 ];
 const col2 = [
     {
         id: 1,
-        name: "GRCG8",
-        pic: GRCG8
+        name: "norway4",
+        pic: norway4
     },
     {
         id: 2,
-        name: "GRCG2",
-        pic: GRCG2
+        name: "norway5",
+        pic: norway5
     },
     {
         id: 3,
-        name: "GRCG5",
-        pic: GRCG5
+        name: "norway6",
+        pic: norway6
+    },
+    {
+        id: 4,
+        name: "norway10",
+        pic: norway10
+    },
+    {
+        id: 5,
+        name: "norway13",
+        pic: norway13
+    },
+    {
+        id: 6,
+        name: "norway16",
+        pic: norway16
+    },
+    {
+        id: 7,
+        name: "norway19",
+        pic: norway19
     },
     
 ];
 const col3 = [
     {
         id: 1,
-        name: "GRCG4",
-        pic: GRCG4
+        name: "norway7",
+        pic: norway7
     },
     {
         id: 2,
-        name: "GRCG6",
-        pic: GRCG6
+        name: "norway8",
+        pic: norway8
+    },
+    {
+        id: 3,
+        name: "norway9",
+        pic: norway9
+    },
+    {
+        id: 4,
+        name: "norway12",
+        pic: norway12
+    },
+    {
+        id: 5,
+        name: "norway15",
+        pic: norway15
+    },
+    {
+        id: 6,
+        name: "norway18",
+        pic: norway18
     },
 ];
     
